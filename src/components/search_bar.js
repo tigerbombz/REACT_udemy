6cfb1de1b88649a9635cdebc4c312e5f88409562
tigerbombz <madd1778@yahoo.com>
2016-03-29 20:07:47 -0700

    init repo for udemy tutorial

---
+++ src/components/search_bar.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+
+class SearchBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = { term: 'Starting Value'};
+  }
+
+  render() {
+    console.log("test",this.state);
+    return (
+      <div>
+        <input
+          value ={this.state.term}
+          onChange={event => this.setState({term: event.target.value})} />
+      </div>
+    );
+  }
+}
+
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, {Component} from 'react';
+import ReactDOM  from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyDKoHqT49AwK2parYRwUzCw5OloqYqUVsM';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//Create a new component. This component should produce HTML
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'broscience'}, (videos) => {
+      this.setState({videos});
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+//Take this component generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
